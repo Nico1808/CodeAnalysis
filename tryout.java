46bed3b7a3629a3e80162d1f7ee8e524e198d7c4
Nico Niebisch <niconiebisch@gmail.com>
2022-06-28 09:24:51 +0200

    try-with-ressources for File to Charset method

--- tryout.java
+++ tryout.java
@@ -1,6 +1,5 @@
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.concurrent.CyclicBarrier;
 import java.util.regex.*;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
@@ -10,16 +9,22 @@ import java.nio.channels.FileChannel;
 public class tryout{
 
     public static CharSequence fromFile(String filename) throws IOException {
-        FileInputStream fis = new FileInputStream(filename);
-        FileChannel fc = fis.getChannel();
-        
-        // Create a read-only CharBuffer on the file --> CharSequence is a read only sequence of chars
-        // fc.Map maps the FileChannel (specified through the args 0: beginning of file to fc.size() (to the end)) into memory = Buffer
-        ByteBuffer bbuf = fc.map(FileChannel.MapMode.READ_ONLY, 0, (int) fc.size());
-        //Since we need a Charsequence for p.matcher(input) we define a Charset (a named mapping between chars and bytes) which is decoded from the bytebuffer
-        CharBuffer cbuf = Charset.forName("UTF-8").newDecoder().decode(bbuf);
-        // cbuf is now a sequence of chars which we created from the byteBuffer, which we created from the FileChannel
-        return cbuf;
+        try(FileInputStream fis = new FileInputStream(filename)){
+
+            FileChannel fc = fis.getChannel();
+            // Create a read-only CharBuffer on the file --> CharSequence is a read only sequence of chars
+            // fc.Map maps the FileChannel (specified through the args 0: beginning of file to fc.size() (to the end)) into memory = Buffer
+            ByteBuffer bbuf = fc.map(FileChannel.MapMode.READ_ONLY, 0, (int) fc.size());
+            //Since we need a Charsequence for p.matcher(input) we define a Charset (a named mapping between chars and bytes) which is decoded from the bytebuffer
+            CharBuffer cbuf = Charset.forName("UTF-8").newDecoder().decode(bbuf);
+            // cbuf is now a sequence of chars which we created from the byteBuffer, which we created from the FileChannel
+            return cbuf;
+            
+        }catch(Exception e){
+            System.out.println("File to CharSequence failed:\n");
+            e.printStackTrace();
+        }
+        return "";
     }
 
 
