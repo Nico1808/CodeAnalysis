1050313603e23d2cd48a3aad4e0a656215e45def
frecherenkel60 <rieger.kevin01@gmail.com>
2022-07-01 13:20:14 +0200

    gui

--- Gui.java
+++ Gui.java
@@ -1,5 +1,28 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import java.awt.*;
 
 public class Gui extends JFrame {
     
+    Gui(String title){
+        super(title);
+    }
+
+    public static void main(String[] args) {
+        // Create main frame
+        var mainFrame = new Gui("Code Analyzer");
+        mainFrame.setLayout(new BorderLayout());
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Create panels
+        var topPanel = new JPanel(new FlowLayout());
+        var centerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        var bottomPanel = new JPanel(new FlowLayout());
+
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+    }
+
 }
